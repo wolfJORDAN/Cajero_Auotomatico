1e0504471e42aae7f47161db70293e24f5d7ea51
Emerson <123414085+wolfJORDAN@users.noreply.github.com>
2023-08-13 19:16:37 -0600

    Update Funcion_menu_principal.cpp

--- Funcion_menu_principal.cpp
+++ Funcion_menu_principal.cpp
@@ -8,20 +8,66 @@ using namespace std;
 
 
 //Funcion encargada de validar la clave
-void validar(std::string clave) {
+bool validar(std::string clave_ingresada) {
 
-    string pass;
+    string clave_correcta;
+
+    clave_correcta = "8526";
 
     /*pass = que busque las claves en el archivo txt*/
 
-    cout<<"Ingrese una clave: ";
-    getline(cin, pass);
-
-    if(pass == clave){
+    if(clave_correcta == clave_ingresada){
         cout<<"Clave valida"<<endl;
+        return true;
+        
+
     }
     else{
         cout<<"Clave incorrecta!"<<endl;
+        return false;
     }
 }
 
+void desplegar_menu(){
+
+    string clave;
+
+    /*cout<<"Ingrese una clave: ";
+    //cin.ignore(); // Limpiar el búfer de entrada
+    getline(cin,clave);
+
+    bool aut = validar(clave);*/
+
+    while(true){
+        cout<<"Ingrese una clave: ";
+            //cin.ignore(); // Limpiar el búfer de entrada
+            getline(cin,clave);
+
+            bool aut = validar(clave);
+        if(aut==true){
+
+            /*cout<<"Ingrese una clave: ";
+            cin.ignore(); // Limpiar el búfer de entrada
+            getline(cin,clave);*/
+
+            //bool aut = validar(clave);
+
+            cout<</*Numero_Cuenta*/"12345678"<<endl;
+            cout<</*Nombre*/"Emerson Jordan"<<endl;
+            cout<</*telefono*/""<<endl;
+            cout<</*Direccion*/"Saspan Chiquimula"<<endl;
+            cout<</*Saldo*/""<<endl;
+            break;
+        }
+    }
+
+}
+
+int main(){
+
+    desplegar_menu();
+
+    system("PAUSE");
+
+    return 0;
+}
